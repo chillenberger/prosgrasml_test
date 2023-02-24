dcdd3bb332f1061030a6c00d68663b08c4499040
Dan <dillenbe@gmail.com>
2023-02-24 11:58:33 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import React from 'react'; 
+import logo from './static/oath.svg'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+          <h3>Create Account</h3>
+          <p className="instructions">Sign up to PostgressML by entering an email and password.</p>
+          <form className="signupForm">
+            <label>Email</label>
+            <input className="textInput" type="text"/>
+            <label>Create Password</label>
+            <input className="textInput" type="text"/>
+            <label>Repeat Password</label>
+            <input className="textInput" type="text"/>
+            <label><input type="checkbox" className="checkbox"/> By creating an account you agree with the Terms of Service and our Privacy Policy</label>
+            <input className="submit" type="submit"/>
+          </form>
+          <div className="icons">
+            <img src={logo}/>
+          </div>
+          <div className="signIn">
+              <p>Have an accont? </p>
+              <button className="signInButton">Sign In</button>
+          </div>
+      </div>
+
+
     </div>
   );
 }
